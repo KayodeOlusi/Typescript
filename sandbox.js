7d3af2a3ed9f9427d77053db6e69fb4a798f6a87
KayodeOusi <kayodeolusi@gmail.com>
2022-03-20 16:19:12 +0100

    different types

--- sandbox.js
+++ sandbox.js
@@ -5,12 +5,32 @@
 // }
 // console.log(circle(7.5));
 // Arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('super');
-// Objects
-var obj = {
-    name: 'mario',
-    age: 23,
-    adult: true
-};
-obj.age = 50;
+// let names = ['luigi', 'mario', 'yoshi']
+// names.push('super')
+// // Objects
+// let obj = {
+//     name: 'mario',
+//     age: 23,
+//     adult: true
+// }
+// obj.age = 50;
+// explicit types 
+var character;
+var age;
+var isFalse;
+// arrays
+var names = []; // define the type and set it to a defualt value
+//union types
+var mixed = []; // to use multiple types in an array
+var uid;
+// objects
+var obj1;
+var obj2;
+var obj3;
+// any
+var anyName; // setting the varaible to have any type
+var mixedNames = [];
+mixedNames.push("Kay");
+mixedNames.push(false);
+mixedNames.push(3);
+var mixedObject;
--- sandbox.ts
+++ sandbox.ts
@@ -9,15 +9,50 @@
 // console.log(circle(7.5));
 
 // Arrays
-let names = ['luigi', 'mario', 'yoshi']
-names.push('super')
+// let names = ['luigi', 'mario', 'yoshi']
+// names.push('super')
 
-// Objects
+// // Objects
 
-let obj = {
-    name: 'mario',
-    age: 23,
-    adult: true
+// let obj = {
+//     name: 'mario',
+//     age: 23,
+//     adult: true
+// }
+
+// obj.age = 50;
+
+// explicit types 
+let character: string;
+let age: number;
+let isFalse: boolean;
+
+// arrays
+let names: string[] = []; // define the type and set it to a defualt value
+
+//union types
+let mixed: (string|number|boolean)[] = []; // to use multiple types in an array
+let uid: string|number;
+
+// objects
+let obj1: {}
+let obj2: object;
+
+let obj3: {
+    name: string,
+    age: number,
+    isAdult: boolean
 }
 
-obj.age = 50;
+// any
+
+let anyName: any; // setting the varaible to have any type
+
+let mixedNames: any[] = [];
+mixedNames.push("Kay");
+mixedNames.push(false);
+mixedNames.push(3);
+
+
+let mixedObject: { name: any, age: any }
+
