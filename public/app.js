af2629efe5fba20d7687fe4749db23f1f955d517
KayodeOusi <kayodeolusi@gmail.com>
2022-03-21 10:03:36 +0100

    classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "for shopping", 500);
+const invTwo = new Invoice("luigi", "for bank", 50);
+let invoices = [];
+invoices.push(invOne, invTwo);
+console.log(invoices);
 const anchor = document.querySelector('a');
 console.log(anchor.href);
 // Use "!" for the element to not be null  
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice("mario", "for shopping", 500);
+const invTwo = new Invoice("luigi", "for bank", 50);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne, invTwo);
+console.log(invoices);
+
+
 const anchor = document.querySelector('a')!;
 console.log(anchor.href);
 // Use "!" for the element to not be null  
